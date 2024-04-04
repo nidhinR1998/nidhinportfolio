feab7b56c2f01791b58c34229f09f3ca2584df81
NIDHIN R <103571954+nidhinR1998@users.noreply.github.com>
2024-04-04 23:02:18 +0530

    Update main.js
    Right click

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-var typed = new Typed(".text", {
-    strings: ["Fullstack Developer", "Web Developer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
+document.addEventListener('DOMContentLoaded', function() {
+    // Disable right-click
+    document.addEventListener('contextmenu', function(event) {
+        event.preventDefault(); // Prevent default right-click behavior
+    });
+    
+    // Typed.js initialization
+    var typed = new Typed(".text", {
+        strings: ["Fullstack Developer", "Web Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+    
+    // Other JavaScript code for your portfolio page
+    // ...
 });
-
-document.oncontextmenu = () => {
-    return false
-}
-
-document.onkeydown = e => {
-    if(e.key == "F12") {
-        return false
-    }
-}
